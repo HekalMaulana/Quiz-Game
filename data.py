82b387686ec4e89d40ff32e7cbbbf068ee4ab1b0
Muhammad Hekal Maulana <maulanaahekal@gmail.com>
2023-07-19 11:34:04 +0800

    Update the data

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 import requests
 
-response = requests.get(url="https://opentdb.com/api.php?amount=10&type=boolean")
+response = requests.get(url="https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=boolean")
 response.raise_for_status()
 data = response.json()
 
